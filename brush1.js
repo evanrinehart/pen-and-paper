b97b894165564c63d6ae231c24305b6cc1983209
Evan Rinehart <evanrinehart@gmail.com>
2023-01-28 18:50:16 -0600

    fix coordinates bug

--- brush1.js
+++ brush1.js
@@ -12,14 +12,14 @@ var brush1Tool = {
   click: function(ev){
     if(!ev.target.classList.contains("surface")) return;
     //draw shape if previous point exists
-    var freeP = eventPositionToStandard(ev);
+    var freeP = eventPositionToStandard(ev); /* ! */
     var p = snapPoint(freeP);
     Brush1.points.push(p);
     Brush1.cursor = p;
     eachCanvasClass("scratch", brush1RefreshIndicator);
   },
   mouseMove: function(ev){
-    var freeP = eventPositionToStandard(ev);
+    var freeP = eventPositionToStandard(ev); /* ! */
     var p = snapPoint(freeP);
     Brush1.cursor = p;
     eachCanvasClass("scratch", brush1RefreshIndicator);
@@ -60,14 +60,14 @@ function brush1RefreshIndicator(canvas, ctx){
   ctx.globalCompositeOperation = "source-over";
 
   if(Brush1.points.length < 1){
-    var c = Brush1.cursor;
+    var c = standardPositionToCanvas(canvas, Brush1.cursor);
     ctx.fillStyle = Brush1.color;
     ctx.arc(c.x + 0.5, c.y + 0.5, Brush1.thickness / 2.0, 0, 2*Math.PI);
     ctx.fill();
   }
   else{
     var points = Brush1.points;
-    var start = points[0];
+    var start = standardPositionToCanvas(canvas, points[0]);
     ctx.beginPath();
     ctx.strokeStyle = Brush1.color;
     ctx.lineWidth = Brush1.thickness;
@@ -76,10 +76,10 @@ function brush1RefreshIndicator(canvas, ctx){
     //ctx.miterLimit = 2;
     ctx.moveTo(start.x, start.y);
     for(let i=1; i<points.length; i++){
-      var q = points[i];
+      var q = standardPositionToCanvas(canvas, points[i]);
       ctx.lineTo(q.x, q.y);
     }
-    var c = Brush1.cursor;
+    var c = standardPositionToCanvas(canvas, Brush1.cursor);
     ctx.lineTo(c.x, c.y);
     ctx.stroke();
   }
@@ -89,7 +89,7 @@ function brush1RefreshIndicator(canvas, ctx){
 function brush1Commit(canvas, ctx){
   if(Brush1.points.length < 2) return;
   var points = Brush1.points;
-  var start = points[0];
+  var start = standardPositionToCanvas(canvas, points[0]);
   ctx.beginPath();
   ctx.globalCompositeOperation = "source-over";
   ctx.strokeStyle = Brush1.color;
@@ -99,7 +99,7 @@ function brush1Commit(canvas, ctx){
   //ctx.miterLimit = 2;
   ctx.moveTo(start.x, start.y);
   for(let i=1; i<points.length; i++){
-    var q = points[i];
+    var q = standardPositionToCanvas(canvas, points[i]);
     ctx.lineTo(q.x, q.y);
   }
   ctx.stroke();
